5af2586d9158e1b79e1b409e069b2da97a56acb1
Jamil Atta Junior <atta.jamil@gmail.com>
2016-09-14 10:22:52 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='legendarium',
-    version='0.1.3',
+    version='0.1.4',
     description="Python library to handle SciELO`s bibliographic legend",
     long_description=readme + '\n\n' + history,
     author="Jamil Atta Junior",
