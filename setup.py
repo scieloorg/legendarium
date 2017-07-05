ce05c3b444df6b1a22cfc71fa41301aed8a3b947
fabiobatalha <fabiobatalha@gmail.com>
2017-07-05 15:10:18 -0300

    Ajusta travis.yml versões de teste

--- setup.py
+++ setup.py
@@ -41,9 +41,6 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
     ],
     test_suite='tests',
     tests_require=test_requirements
